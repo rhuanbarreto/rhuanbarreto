67d6012c12f065fd51f5c04b481929685d72b46d
Rhuan Barreto <rhuan@rhuan.com.br>
2022-12-26 01:47:10 -0300

    infra: add chromatic (#6)
    * infra: add chromatic
    
    * add workflow for running test on deployment
    
    * Update workflows

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/Home/Home";
+import Home from "./pages/Home/Home";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
