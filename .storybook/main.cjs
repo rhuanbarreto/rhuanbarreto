5581c4f2c94569d1dd9cf4d6ed37affdca3df1bc
Rhuan Barreto <rhuan.barreto@cognite.com>
2022-12-29 00:19:50 -0300

    Fix everything

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,26 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-coverage",
     {
-      name: 'storybook-addon-sass-postcss',
+      name: "storybook-addon-sass-postcss",
       options: {
         loadSassAfterPostCSS: true,
       },
     },
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true,
+  features: {
+    storyStoreV7: true,
     previewCsfV3: true,
-  }
-}
+  },
+};
--- src/components/StickyMenu/StickyMenu.tsx
+++ src/components/StickyMenu/StickyMenu.tsx
@@ -1,20 +1,23 @@
-import { HTMLAttributes, PropsWithChildren, ReactNode } from "react";
+import { FC, HTMLAttributes, PropsWithChildren, ReactNode } from "react";
 
 interface Props extends PropsWithChildren {
-  links?: ({ component: ReactNode } | HTMLAttributes<HTMLAnchorElement>)[];
+  links?: Array<{ component: ReactNode } | HTMLAttributes<HTMLAnchorElement>>;
 }
 
-export const StickyMenu = ({ links = [], children }: Props) => {
+export const StickyMenu: FC<Props> = ({ links = [], children }: Props) => {
   return (
     <nav className="bg-gray-800 sticky w-full z-10 top-0 shadow">
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div className="pl-4 flex items-center">
-          {links.map((link) => {
+          {links.map((link, i) => {
             if ("component" in link) return link.component;
             return (
               <a
+                key={i}
                 {...link}
-                className={`text-white text-base no-underline hover:text-gray-100 hover:no-underline ml-4 first:ml-0 ${link.className}`}
+                className={`text-white text-base no-underline hover:text-gray-100 hover:no-underline ml-4 first:ml-0 ${
+                  link.className ?? ""
+                }`}
               />
             );
           })}
