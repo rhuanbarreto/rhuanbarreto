e1f6f2df97956186b8d499b9e5b36f572c9a51f6
Rhuan Barreto <rhuan.barreto@cognite.com>
2022-12-29 00:22:56 -0300

    Fix problem

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,34 +1,34 @@
 const { mergeConfig } = require("vite");
-const turbosnap = require('vite-plugin-turbosnap');
+const turbosnap = require("vite-plugin-turbosnap");
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-coverage",
     {
-      name: 'storybook-addon-sass-postcss',
+      name: "storybook-addon-sass-postcss",
       options: {
         loadSassAfterPostCSS: true,
       },
     },
   ],
-  "framework": "@storybook/react",
-  "core": {
-    builder: "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
+  features: {
     storyStoreV7: true,
     previewCsfV3: true,
   },
   async viteFinal(config, { configType }) {
     return mergeConfig(config, {
-      plugins: configType === 'PRODUCTION' ? [turbosnap.default({ rootDir: config.root ?? process.cwd() })] : [],
+      plugins:
+        configType === "PRODUCTION"
+          ? [turbosnap({ rootDir: config.root ?? process.cwd() })]
+          : [],
     });
   },
-}
+};
