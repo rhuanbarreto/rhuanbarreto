a316649c9752c2becf243a1be1a4ce573f57bb66
Rhuan Barreto <rhuan.barreto@cognite.com>
2023-01-02 11:28:21 -0300

    Add more content to home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,38 @@
 import { FC } from "react";
 import { HeroSection } from "../../components/HeroSection/HeroSection";
 import { StickyMenu } from "../../components/StickyMenu/StickyMenu";
+import profile from "../../assets/profile.jpg";
 
-export const Home: FC = () => {
-  return (
-    <>
-      <HeroSection title="Rhuan Samary Barreto" sectionProps={{ id: "main" }}>
-        Hi! Wanna know more about me?
-      </HeroSection>
-      <StickyMenu
-        links={[
-          { children: "About Me" },
-          { children: "Career" },
-          { children: "Blogs" },
-          { children: "Get in Touch" },
-        ]}
-      />
-    </>
-  );
-};
+export const Home: FC = () => (
+  <>
+    <HeroSection
+      title="Rhuan Samary Barreto"
+      sectionProps={{ id: "main" }}
+      buttonProps={{ children: "Learn More", href: "#about" }}
+    >
+      Hi! Wanna know more about me?
+    </HeroSection>
+    <StickyMenu links={[{ children: "About Me", href: "#about" }]} />
+    <HeroSection
+      title="About Me"
+      sectionProps={{ id: "about" }}
+      buttonProps={{ children: "Know More About my Career", href: "#career" }}
+    >
+      <div className="grid grid-cols-2 gap-8 items-center">
+        <div>
+          <img src={profile} />
+        </div>
+        <div>
+          <p className="mb-4">
+            I&#39;m an engineer that loves to solve problems using technology.
+          </p>
+          <p className="mb-4">
+            Programming is the tool I use the most for solving them. But my life
+            is/was not only about programming. I started programming very early,
+            since I was 10 years old.
+          </p>
+        </div>
+      </div>
+    </HeroSection>
+  </>
+);
