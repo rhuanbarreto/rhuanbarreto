274a76f311190e789cbe2f26f44ccacdcc01d53f
Rhuan Barreto <rhuan@rhuan.com.br>
2023-01-05 13:59:46 -0300

    feat: improve component generator (#23)

--- templates/component/index.js
+++ templates/component/index.js
@@ -4,25 +4,40 @@ import { resolve } from "path";
 import Mustache from "mustache";
 
 const componentsFolder = resolve("src/components");
+const pagesFolder = resolve("src/pages");
 
 inquirer
   .prompt([
     {
+      type: "confirm",
+      name: "page",
+      message: "Is this a page component?",
+      default: false,
+    },
+    {
       type: "input",
       name: "name",
       message: "What's the component name?",
-      validate(value) {
+      validate(value, answers) {
         const pass = value.match(/^[A-Z][A-Za-z0-9]*$/g);
         if (!pass)
           return "Please enter a valid component name. It must start with a capital letter.";
-        const filePath = `${componentsFolder}/${value}/${value}.tsx`;
+        const folder = answers.page ? pagesFolder : componentsFolder;
+        const filePath = `${folder}/${value}/${value}.tsx`;
         if (existsSync(filePath))
           return "Component already exists! It won't be overwritten!";
         return true;
       },
     },
+    {
+      type: "confirm",
+      name: "props",
+      message: "Does it have props?",
+      default: true,
+      when: (answers) => !answers.page,
+    },
   ])
-  .then(({ name }) => {
+  .then((answers) => {
     const componentTemplate = readFileSync(
       resolve("templates/component/component.mustache"),
       "utf-8"
@@ -31,12 +46,15 @@ inquirer
       resolve("templates/component/stories.mustache"),
       "utf-8"
     );
-    const componentOutput = Mustache.render(componentTemplate, { name });
-    const storiesOutput = Mustache.render(storiesTemplate, { name });
-    mkdirSync(`${componentsFolder}/${name}`);
-    const componentFile = `${componentsFolder}/${name}/${name}.tsx`;
+    const templateVars = { curly: "{", ...answers };
+    const componentOutput = Mustache.render(componentTemplate, templateVars);
+    const storiesOutput = Mustache.render(storiesTemplate, templateVars);
+    const { name, page } = answers;
+    const folder = page ? pagesFolder : componentsFolder;
+    mkdirSync(`${folder}/${name}`);
+    const componentFile = `${folder}/${name}/${name}.tsx`;
     writeFileSync(componentFile, componentOutput);
-    const storiesFile = `${componentsFolder}/${name}/${name}.stories.tsx`;
+    const storiesFile = `${folder}/${name}/${name}.stories.tsx`;
     writeFileSync(storiesFile, storiesOutput);
     console.log(`Component ${name} created successfully`);
   })
